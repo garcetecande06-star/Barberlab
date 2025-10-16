67877ac8abd3a0e6f1660239c6803828cefe654b
garcetecande06-estrella <garcetecande06@gmail.com>
2025-10-16 19:46:17 -0300

    restriccion agenda

--- apps/barbero/views.py
+++ apps/barbero/views.py
@@ -1,10 +1,47 @@
+from django.views import View
 from django.views.generic import TemplateView
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from apps.turno.models import Turno
 from apps.barbero.models import Barbero
 
-class AgendaTurnosView(TemplateView):
+class LoginStaffView(View):
+    template_name = 'Inicio_De_Sesion_Barberos.html'  
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        #Se obtienen los datos del formulario
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            #Si el usuario existe y es staff
+            if user.is_staff:
+                #Se inicia sesión
+                login(request, user)
+                #Se redirige a la agenda
+                return redirect('agenda')  # Redirige a la agenda
+            else:
+                #El usuario no es staff y no tiene acceso
+                messages.error(request, "No tienes permisos para acceder.")
+        else:
+            #Usuario o contraseña incorrectos
+            messages.error(request, "Usuario o contraseña incorrectos.")
+        #Luego de un error muestra el formulario de nuevo
+        return render(request, self.template_name)
+
+
+class AgendaTurnosView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
     template_name = 'agendaBarbero.html'
+    login_url = 'login_staff' 
+
+    def test_func(self):
+        return self.request.user.is_staff  # Solo staff puede acceder
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -19,6 +56,3 @@ class AgendaTurnosView(TemplateView):
 
         context['barberos'] = Barbero.objects.all()
         return context
-
-def acceso_generico_view(request):
-    return render(request, 'Inicio_De_Secion_Barberos.html')
--- barberlab/urls.py
+++ barberlab/urls.py
@@ -1,22 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from apps.cliente.views import IndexView, RegistroClienteView, LoginClienteView, TurnosClienteView, reservarTurnoView, eliminarTurnoView
-from apps.barbero.views import AgendaTurnosView, acceso_generico_view 
+from apps.barbero.views import AgendaTurnosView, LoginStaffView
 from apps.valoracion.views import NuevaValoracionView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
     path('nuevaValoracion/', NuevaValoracionView.as_view(), name='nuevaValoracion'),
-    
-    path('acceso/', acceso_generico_view, name='acceso_gen'),
-    
-    # RUTA DE DESTINO: La agenda
-    path('agenda/', AgendaTurnosView.as_view(), name='agenda_turnos'),
-    
+    path('acceso/', LoginStaffView.as_view(), name='login_staff'), 
+    path('agenda/', AgendaTurnosView.as_view(), name='agenda'),
     path('login/', LoginClienteView.as_view(), name='login'),
-    path('registro/', RegistroClienteView.as_view(), name='logout'),
-    path('turnoCliente/',TurnosClienteView.as_view(), name='turnosCliente' ),
+    path('registro/', RegistroClienteView.as_view(), name='registro'),
+    path('turnoCliente/', TurnosClienteView.as_view(), name='turnosCliente'),
     path('reservarTurno/', reservarTurnoView.as_view(), name='turno_nuevo'),
     path('turno/<int:pk>/delete/', eliminarTurnoView.as_view(), name='eliminarTurno'),
 ]
