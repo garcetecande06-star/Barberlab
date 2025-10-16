b7a8e07e853db8bc7bfa6a4d0223341c305309b2
dan <Daniel123@gmail.com>
2025-10-16 11:08:32 -0300

    Restriciones de agenda de barberos

--- barberlab/settings.py
+++ barberlab/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'NAME': 'Barberlab',
         'USER': '',
         'PASSWORD': '',
-        'HOST': r'EQUIPO\SQLEXPRESS',
+        'HOST': r'CANDE\SQLEXPRESS',
         'PORT': '',
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',
